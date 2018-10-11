6b0314a5eed25cdfa4865a741d71f9a3d11f49c0
livingstonb@uchicago.edu <ak.brian09@gmail.com>
2018-10-11 12:44:38 -0500

    Changed temptation back

--- egp_AR1_IID_tax_recode.m
+++ egp_AR1_IID_tax_recode.m
@@ -111,15 +111,15 @@ function [sim_results,direct_results,norisk_results,checks,decomp] ...
     
     %% UTILITY FUNCTION, BEQUEST FUNCTION
     if p.risk_aver==1
-        prefs.u = @(c) (1 + p.temptation) * log(c);
+        prefs.u = @(c)log(c);
         prefs.beq = @(a) p.bequest_weight.* log(a+ p.bequest_luxury);
     else    
-        prefs.u = @(c) (1 + p.temptation) * (c.^(1-p.risk_aver)-1)./(1-p.risk_aver);
+        prefs.u = @(c)(c.^(1-p.risk_aver)-1)./(1-p.risk_aver);
         prefs.beq = @(a) p.bequest_weight.*((a+p.bequest_luxury).^(1-p.risk_aver)-1)./(1-p.risk_aver);
     end    
 
-    prefs.u1 = @(c) (1 + p.temptation) * c.^(-p.risk_aver);
-    prefs.u1inv = @(u) (u/(1+p.temptation)).^(-1./p.risk_aver);
+    prefs.u1 = @(c) c.^(-p.risk_aver);
+    prefs.u1inv = @(u) u.^(-1./p.risk_aver);
     prefs.beq1 = @(a) p.bequest_weight.*(a+p.bequest_luxury).^(-p.risk_aver);
 
     %% MODEL SOLUTION
