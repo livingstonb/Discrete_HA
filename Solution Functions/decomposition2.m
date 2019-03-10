70dcaa3705fba4c6d02eeeecb582f85f5a3a6c14
Brian Livingston <livingstonb@uchicago.edu>
2019-03-09 22:58:14 -0600

    Switched decomp2 to use only E[MPC|a], fixed RA decomp to be evalued at a=3.5, interpolated

--- Solution Functions/decomposition2.m
+++ Solution Functions/decomposition2.m
@@ -18,6 +18,7 @@ function [decomp2,decomp3] = decomposition2(params,results)
         end
     end
 
+    agrid_short = agrid;
     agrid = repmat(agrid,params(1).nyP*params(1).nyF*params(1).nb,1);
 
     % Initialize with NaNs
@@ -40,10 +41,10 @@ function [decomp2,decomp3] = decomposition2(params,results)
         decomp3(ip).mpc4_term2 = NaN;
         decomp3(ip).mpc4_term3 = NaN;
 
-        decomp2(ip).mpc1_term3a = NaN(numel(params(1).abars),1);
-        decomp2(ip).mpc1_term3b = NaN(numel(params(1).abars),1);
-        decomp2(ip).mpc4_term3a = NaN(numel(params(1).abars),1);
-        decomp2(ip).mpc4_term3b = NaN(numel(params(1).abars),1);
+        decomp2(ip).mpc1_term2a = NaN(numel(params(1).abars),1);
+        decomp2(ip).mpc1_term2b = NaN(numel(params(1).abars),1);
+        decomp2(ip).mpc4_term2a = NaN(numel(params(1).abars),1);
+        decomp2(ip).mpc4_term2b = NaN(numel(params(1).abars),1);
 
         decomp2(ip).decomp_error = NaN;
         decomp3(ip).decomp_error = NaN;
@@ -55,137 +56,133 @@ function [decomp2,decomp3] = decomposition2(params,results)
             baseind = find(ismember({params.name},{'baseline_Q'}));
         end
         
-        g1 = results(ip).direct.adist(:);       % model distribution
-        
-        g1_nb = reshape(g1,[],params(ip).nb);
-        g1 = sum(g1_nb,2);
-        
-        g1_nF = reshape(g1,[],params(ip).nyF);
-        g1 = sum(g1_nF,2);
-        
-        g1_nonzero = g1 .* (g1>1e-9);
-        g1_nonzero(g1_nonzero==0) = 2; % avoid dividing by zero
+        %% baseline model
+        g0 = results(baseind).direct.adist(:); 
 
+        % g0_wide rows indexed by assets, columns by yF, yP, beta combination
+        dim0 = params(1).nyF * params(1).nyP * params(1).nb;
+        g0_wide = reshape(g0,[params(1).nxlong dim0]);
 
-        g0 = results(baseind).direct.adist(:);  % baseline distribution
+        % P(a), vector
+        g0_a = sum(g0_wide,2);
 
+        % Avoid dividing by very small numbers
+        g0small = g0_a < 1e-9;
+        g0_a_nonzero = g0_a .* (~g0small) + 2 .* g0small;
+
+        % baseline MPCs
+        m0 = results(1).direct.mpcs(5).mpcs_1_t{1};       
+        m0_wide = reshape(m0,[params(1).nxlong dim0]);
+        m0_a = m0_wide .* g0_wide ./ g0_a_nonzero;
+        m0_a = sum(m0_a,2);
+
+        % P(a) too small to divide by accurately, use arithmetic mean
+        m0_a(g0small) = mean(m0_wide(g0small,:),2);
+
+        m0_4 = 0;
+        for t = 1:4
+            m0_4 = m0_4 + results(1).direct.mpcs(5).mpcs_1_t{t};
+        end
+        m0_4_wide = reshape(m0_4,[params(1).nxlong dim0]);
+        m0_4_a = m0_4_wide .* g0_wide ./ g0_a_nonzero;
+        m0_4_a = sum(m0_4_a,2);
+
+        % P(a) too small to divide by accurately, use arithmetic mean
+        m0_4_a(g0small) = mean(m0_4_wide(g0small,:),2);
+        
+        %% Model 'ip'
+        g1 = results(ip).direct.adist(:);
+        
+        dim1 = params(ip).nyF * params(ip).nyP * params(ip).nb;
+        
+        % rows indexed by assets, columns by yF, yP, beta combination
+        g1_wide = reshape(g1,[params(ip).nxlong dim1]);
+        
+        % P(a), vector
+        g1_a = sum(g1_wide,2);
+        
+        % Avoid dividing by very small numbers
+        g1small = g1_a < 1e-9;
+        g1_a_nonzero = g1_a .* (~g1small) + 2 .* g1small;
+
+        
         %% 1-Period MPC decomposition2 (decomp with respect to baseline)
         
         % model MPCs
-        m1 = results(ip).direct.mpcs(5).mpcs_1_t{1};        
+        m1 = results(ip).direct.mpcs(5).mpcs_1_t{1};       
+        m1_wide = reshape(m1,[params(ip).nxlong dim1]);
+        m1_a = m1_wide .* g1_wide ./ g1_a_nonzero;
+        m1_a = sum(m1_a,2);
         
-        if params(ip).nb > 1
-            % take mpc mean over beta for each (ix,iyP,iyF) point in state space
-            m1_nb = reshape(m1,[],params(ip).nb);
-            
-            % take expectation over beta
-            m1 = m1_nb .* (g1_nb ./ g1_nonzero);
-            m1 = sum(m1,2);
-            
-            % if density was too low at some points, just use arithmetic
-            % mean as mpc
-            m1(g1_nonzero==2) = sum(m1_nb(g1_nonzero==2,:),2) / params(ip).nb;
-        end
-        
-        if params(ip).nyF > 1
-            % take mpc mean over beta for each (ix,iyP,iyF) point in state space
-            m1_nF = reshape(m1,[],params(ip).nyF);
-            m1 = m1_nF .* (g1_nF ./ g1_nonzero);
-            m1 = sum(m1,2);
-            
-            % if density was too low at some points, just use arithmetic
-            % mean as mpc
-            m1(g1_nonzero==2) = sum(m1_nF(g1_nonzero==2,:),2) / params(ip).nyF;
-        end
-        
-        m0 = results(baseind).direct.mpcs(5).mpcs_1_t{1};   % baseline MPCs
+        % P(a) too small to divide by accurately, use arithmetic mean
+        m1_a(g1small) = mean(m1_wide(g1small,:),2);
 
         % Mean model MPC  - mean baseline MPC
         decomp2(ip).mpc1_Em1_less_Em0 = results(ip).direct.mpcs(5).avg_s_t{1,1} ...
                         - results(baseind).direct.mpcs(5).avg_s_t{1,1};
-        decomp2(ip).mpc1_term1 = g0' * (m1 - m0); % Effect of MPC function
-        decomp2(ip).mpc1_term2 = m0' * (g1 - g0); % Effect of distribution
-        decomp2(ip).mpc1_term3 = (m1 - m0)' * (g1 - g0); % Interaction
+        decomp2(ip).mpc1_term1 = g0_a' * (m1_a - m0_a); % Effect of MPC function
+        decomp2(ip).mpc1_term2 = m0_a' * (g1_a - g0_a); % Effect of distribution
+        decomp2(ip).mpc1_term3 = (m1_a - m0_a)' * (g1_a - g0_a); % Interaction
 
         % Decomposition of distribution effect
         for ia = 1:numel(params(ip).abars)
             abar = params(ip).abars(ia); % HtM threshold
-            idx = agrid <= abar;
+            idx = agrid_short <= abar;
             % HtM households
-            decomp2(ip).mpc1_term3a(ia) = m0(idx)' * (g1(idx) - g0(idx));
+            decomp2(ip).mpc1_term2a(ia) = m0_a(idx)' * (g1_a(idx) - g0_a(idx));
             % Non-HtM households
-            decomp2(ip).mpc1_term3b(ia) = m0(~idx)' * (g1(~idx) - g0(~idx));
+            decomp2(ip).mpc1_term2b(ia) = m0_a(~idx)' * (g1_a(~idx) - g0_a(~idx));
         end
         
         %% 1-Period MPC decomposition3 (decomp with respect to representative agent model (RA))
         if params(ip).nb == 1
-            % model MPCs
-            m1 = results(ip).direct.mpcs(5).mpcs_1_t{1};
-            
-            if params(ip).nyF > 1
-                % take mpc mean over beta for each (ix,iyP,iyF) point in state space
-                m1_nF = reshape(m1,[],params(ip).nyF);
-                m1 = m1_nF .* (g1_nF ./ g1_nonzero);
-                m1 = sum(m1,2);
-                
-                % if density was too low at some points, just use arithmetic
-                % mean as mpc at these points
-                m1(g1_nonzero==2) = sum(m1_nF(g1_nonzero==2,:),2) / params(ip).nyF;
-            end
-        
             % RA MPC
             m0 = params(ip).R * (results(ip).direct.beta*params(ip).R)^(-1/params(ip).risk_aver) - 1;
 
-            % expected value of m1 conditional on assets = 0
-            m1_at0 = m1(agrid(:)==0)' * g1(agrid(:)==0) / sum(g1(agrid(:)==0));
+            % To get E[MPC|a=3.5], interpolate
+            m1_a_interp = griddedInterpolant(agrid_short,m1_a,'linear');
+            m1_atmean = m1_a_interp(3.5);
 
             % Mean model MPC - RA MPC
             decomp3(ip).mpc1_Em1_less_mRA = results(ip).direct.mpcs(5).avg_s_t{1,1} - m0;
 
-            decomp3(ip).mpc1_term1 = m1_at0- m0; % Effect of MPC function
+            decomp3(ip).mpc1_term1 = m1_atmean - m0_a; % Effect of MPC function
             decomp3(ip).mpc1_term2 = 0; % Effect of distribution
-            decomp3(ip).mpc1_term3 = (m1 - m0)' * g1 - (m1_at0 - m0); % Interaction
+            decomp3(ip).mpc1_term3 = (m1_a - m0_a)' * g1_a - (m1_atmean - m0_a); % Interaction
         end
         
 
         %% 4-Period MPC decomp2 (decomp with respect to baseline)
         % Only used for quarterly models
         if params(ip).freq == 4
-            m1 = 0;
-            m0 = 0;
+            m1_4 = 0;
             for i = 1:4
                 % model MPCs
                 m1_it = results(ip).direct.mpcs(5).mpcs_1_t{i};
-
-                if params(ip).nb > 1
-                    m1_nb = reshape(m1_it,[],params(ip).nb);
-                    m1_it = m1_nb .* g1_nb ./ g1_nonzero;
-                    m1_it = sum(m1_it,2);
-                    
-                    % if density was too low at some points, just use arithmetic
-                    % mean as mpc
-                    m1_it(g1_nonzero==2) = sum(m1_nb(g1_nonzero==2,:),2) / params(ip).nb;
-                end
-
-                m1 = m1 + m1_it;
-
-                % baseline MPCs
-                m0 = m0 + results(baseind).direct.mpcs(5).mpcs_1_t{i};
+                m1_4 = m1_4 + m1_it;
             end
+            
+            m1_4_wide = reshape(m1_4,[params(ip).nxlong dim1]);
+            m1_4_a = m1_4_wide .* g1_wide ./ g1_a_nonzero;
+            m1_4_a = sum(m1_4_a,2);
+            
+            % P(a) too small to divide by accurately, use arithmetic mean
+            m1_4_a(g1small) = mean(m1_4_wide(g1small,:),2);
 
             decomp2(ip).mpc4_Em1_less_Em0 = results(ip).direct.mpcs(5).avg_1_1to4 ...
                         - results(baseind).direct.mpcs(5).avg_1_1to4;
-            decomp2(ip).mpc4_term1 = g0' * (m1 - m0);
-            decomp2(ip).mpc4_term2 = m0' * (g1 - g0);
-            decomp2(ip).mpc4_term3 = (m1 - m0)' * (g1 - g0);
+            decomp2(ip).mpc4_term1 = g0_a' * (m1_4_a - m0_4_a);
+            decomp2(ip).mpc4_term2 = m0_4_a' * (g1_a - g0_a);
+            decomp2(ip).mpc4_term3 = (m1_4_a - m0_4_a)' * (g1_a - g0_a);
 
             for ia = 1:numel(params(ip).abars)
                 abar = params(ip).abars(ia);
-                idx = agrid <= abar;
-                decomp2(ip).mpc4_term3a(ia) = m0(idx)' * (g1(idx) - g0(idx));
-                decomp2(ip).mpc4_term3b(ia) = m0(~idx)' * (g1(~idx) - g0(~idx));
+                idx = agrid_short <= abar;
+                decomp2(ip).mpc4_term2a(ia) = m0_4_a(idx)' * (g1_a(idx) - g0_a(idx));
+                decomp2(ip).mpc4_term2b(ia) = m0_4_a(~idx)' * (g1_a(~idx) - g0_a(~idx));
             end
         end
             
 
     end
+end
