100b4c0184b7625b20c02cfc45ff61e4fb5b172d
Brian Livingston <ak.brian09@gmail.com>
2019-03-11 10:32:01 -0500

    Added more mpcs_sim trials

--- master.m
+++ master.m
@@ -9,7 +9,7 @@ runopts.Display = 1;
 runopts.Server = 1; % use server paths
 runopts.fast = 0; % very small asset and income grids for speed
 runopts.Simulate = 0;
-runopts.localdir = '/home/brian/Documents/GitHub/Discrete_HA';
+runopts.localdir = '/Users/brianlivingston/Documents/GitHub/Discrete_HA';
 runopts.mpcshocks_after_period1 = 0; % compute mpcs for is > 1?
 
 % local grid tests, 0 to turn off, 1 for transition probs, 2 for simulations
